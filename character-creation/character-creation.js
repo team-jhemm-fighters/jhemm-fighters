15876eaf23bb8eba33bbb404a81be6eb25351fcf
Max Lamb <lordmaxlamb@gmail.com>
2020-04-20 16:15:41 -0700

    localStorage

--- character-creation/character-creation.js
+++ character-creation/character-creation.js
@@ -1,5 +1,5 @@
 import classes from '../data/classes.js';
-
+import { findById, setPlayerProfile } from '../common/utils.js';
 const playerForms = document.getElementById('character-selector');
 
 
@@ -9,11 +9,38 @@ const playerForms = document.getElementById('character-selector');
 playerForms.addEventListener('submit', (event) => {
     event.preventDefault();
     const playerData = new FormData(playerForms);
-    const player1Character = playerData.get('characters1');
-    const player2Character = playerData.get('characters2');
+    
+    makeUsers(playerData);
+
+}   
+);
+
+function makeUsers(playerData) {
+    
+    const player1Class = playerData.get('class1');
+    const player2Class = playerData.get('class2');
+
     const player1Name = playerData.get('player1name');
     const player2Name = playerData.get('player2name');
-    // makeusers in localstorage
-    // const
+
+    const player1Character = findById(classes, player1Class);
+    const player2Character = findById(classes, player2Class);
+
+    const playerObject1 = { 
+        name: player1Name,
+        class: player1Class,
+        health: player1Character.baseHp,
+        energy: player1Character.baseEnergy
+
+    };
+    const playerObject2 = { 
+        name: player2Name,
+        class: player2Class,
+        health: player2Character.baseHp,
+        energy: player2Character.baseEnergy
     
-});
+
+    };
+    setPlayerProfile('player1', playerObject1);
+    setPlayerProfile('player2', playerObject2);
+}
---
+++ common/utils.js
@@ -0,0 +1,15 @@
+export function findById(array, id) {
+    for (let index = 0; index < array.length; index++) {
+        const item = array[index];
+        if (item.id === id) {
+            return item;
+        }
+    }
+
+    return null;
+}
+
+export function setPlayerProfile(string, object) {
+    const temp = JSON.stringify(object);
+    localStorage.setItem(string, temp);
+}
