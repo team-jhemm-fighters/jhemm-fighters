2b4a2b284d7ce77bc618398bb653dd9343d4193d
smooto <melissasmoot@live.com>
2020-04-21 10:23:46 -0700

    update turn logic to include turn type

--- battlefield/battlefield.js
+++ battlefield/battlefield.js
@@ -1,35 +1,39 @@
 import { getLocalStorage } from '../common/utils.js';
 const navigateButton = document.getElementById('navigate-button');
 
-
-
 const player1 = getLocalStorage('player1');
 const player2 = getLocalStorage('player2');
 
+// let turnCounter = 0;
+
 let nextPlayer = 'player1';
+let nextTurn = 'attack';
 
 if (!player1.hasAttacked && !player2.hasAttacked) {
     const flip = Math.random();
     if (flip > .5) {
         nextPlayer = 'player2';
     }
-} else if (player1.hasAttacked && !player2.hasAttacked) {
+    navigateButton.textContent = 'Go to: ' + nextPlayer;
+} else if (player1.hasAttacked && !player2.hasDefended) {
     nextPlayer = 'player2';
-} else if (player2.hasAttacked && !player1.hasAttacked) {
+    nextTurn = 'defend';
+    navigateButton.textContent = 'Go to: ' + nextPlayer;
+} else if (player2.hasAttacked && !player1.hasDefended) {
     nextPlayer = 'player1';
+    nextTurn = 'defend';
+    navigateButton.textContent = 'Go to: ' + nextPlayer;
 } else {
-    nextPlayer = 'Battle!';
+    nextTurn = 'Battle!';
+    navigateButton.textContent = nextTurn;
 }
-
-navigateButton.textContent = 'Go to: ' + nextPlayer;
-
     
 navigateButton.addEventListener('click', () => {
    
     if (nextPlayer === 'player1') {
-        location.href = '../player.html?id=player1';
+        location.href = '../player.html?id=player1&turn=' + nextTurn;
     } else if (nextPlayer === 'player2') {
-        location.href = '../player.html?id=player2';
+        location.href = '../player.html?id=player2&turn=' + nextTurn;
     } else {
         console.log(`here's the battlefield!!!!`);
         /* set view to battlefield */
@@ -37,3 +41,13 @@ navigateButton.addEventListener('click', () => {
     
         // window.location.href = '../player.html';
 });
+
+// battlefield functionality
+
+// update DOM to show battlefield elements
+
+// execute actions from players' turns
+
+// calculate results
+
+// only after BOTH players have attacked AND defended, reset counter to 0(?), reset players' actions in profile and action lS objects
--- player/player.js
+++ player/player.js
@@ -3,21 +3,28 @@ import { getLocalStorage, setActions, setPlayerProfile } from '../common/utils.j
 const button = document.getElementById('submit-action');
 const searchParams = new URLSearchParams(window.location.search);
 const playerId = searchParams.get('id');
+const currentTurn = searchParams.get('turn');
+
 const playername = document.getElementById('name');
 
 const currentPlayer = getLocalStorage(playerId);
 
 playername.textContent = currentPlayer.name;
 
-
+// generate elements dynamically based on currentPlayer and currentTurn
+// currentPlayer for moveset & stats
+// 
 
 button.addEventListener('click', () => {
     const selectedAction = document.querySelector('input[type=radio]:checked');
 
-    currentPlayer.hasAttacked = true;
+    if (currentTurn === 'attack') {
+        currentPlayer.hasAttacked = true;
+    } else {
+        currentPlayer.hasDefended = true;
+    }
     setPlayerProfile(playerId, currentPlayer);
     setActions(selectedAction.value);
 
     window.location.href = '../battlefield.html';
-   
 });
