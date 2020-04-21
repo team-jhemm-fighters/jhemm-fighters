e0ff9f4d39b639614ec47793dbedcbfd7e9d8722
Hunter Danielson <hunterleedanielson@gmail.com>
2020-04-20 17:00:23 -0700

    battlefield nav button/coinflip

--- battlefield/battlefield.js
+++ battlefield/battlefield.js
@@ -1,14 +1,29 @@
 import { getLocalStorage } from '../common/utils.js';
 const navigateButton = document.getElementById('navigate-button');
 
+
+
 const player1Actions = getLocalStorage();
 const player2Actions = getLocalStorage();
 
+let firstPlayer = 'player1';
+
 if (!player1Actions && !player2Actions) {
-    let coin = 'tails';
     const flip = Math.random();
     if (flip > .5) {
-        coin = 'heads';
+        firstPlayer = 'player2';
     }
-    console.log(coin);
 }
+navigateButton.textContent = 'Go to: ' + firstPlayer;
+
+    
+navigateButton.addEventListener('click', () => {
+   
+    if (firstPlayer === 'player1') {
+        location.href = '../player.html?id=player1';
+    } else if (firstPlayer === 'player2') {
+        location.href = '../player.html?id=player2';
+    }
+    
+        // window.location.href = '../player.html';
+});
