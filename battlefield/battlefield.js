9e0ab442a83089bb972dc144379765effa351d76
smooto <melissasmoot@live.com>
2020-04-21 09:48:01 -0700

    finish basic turn logic functionality

--- battlefield/battlefield.js
+++ battlefield/battlefield.js
@@ -6,23 +6,33 @@ const navigateButton = document.getElementById('navigate-button');
 const player1 = getLocalStorage('player1');
 const player2 = getLocalStorage('player2');
 
-let firstPlayer = 'player1';
+let nextPlayer = 'player1';
 
 if (!player1.hasAttacked && !player2.hasAttacked) {
     const flip = Math.random();
     if (flip > .5) {
-        firstPlayer = 'player2';
+        nextPlayer = 'player2';
     }
+} else if (player1.hasAttacked && !player2.hasAttacked) {
+    nextPlayer = 'player2';
+} else if (player2.hasAttacked && !player1.hasAttacked) {
+    nextPlayer = 'player1';
+} else {
+    nextPlayer = 'Battle!';
 }
-navigateButton.textContent = 'Go to: ' + firstPlayer;
+
+navigateButton.textContent = 'Go to: ' + nextPlayer;
 
     
 navigateButton.addEventListener('click', () => {
    
-    if (firstPlayer === 'player1') {
+    if (nextPlayer === 'player1') {
         location.href = '../player.html?id=player1';
-    } else if (firstPlayer === 'player2') {
+    } else if (nextPlayer === 'player2') {
         location.href = '../player.html?id=player2';
+    } else {
+        console.log(`here's the battlefield!!!!`);
+        /* set view to battlefield */
     }
     
         // window.location.href = '../player.html';
