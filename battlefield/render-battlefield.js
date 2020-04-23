b5a7e67ecfd2cfe52b28b968d65ae4307020a07c
smooto <melissasmoot@live.com>
2020-04-22 22:36:49 -0700

    prep battlefield html and app to prep for styling and UX improvements

--- battlefield/render-battlefield.js
+++ battlefield/render-battlefield.js
@@ -3,6 +3,27 @@ import { applyDamage, calculateDamage, calculateEnergy, isDead } from '../battle
 import attacks from '../data/attack.js';
 import defense from '../data/defense.js';
 
+// DOM elements for updated stat rendering, identical to elements/variables used on player page -- can probably be refactored along with player page dom manipulation into a renderStats() utility of some kind
+const player1name = document.getElementById('player-1-name');
+const player1health = document.getElementById('player-1-health');
+const player1energy = document.getElementById('player-1-energy');
+
+const player2name = document.getElementById('player-2-name');
+const player2health = document.getElementById('player-2-health');
+const player2energy = document.getElementById('player-2-energy');
+
+// here's a very basic renderStats() function, to get refactoring started
+function renderStats() {
+    player1name.textContent = player1.name;
+    player1health.textContent = player1.health;
+    player1energy.textContent = player1.energy;
+
+    player2name.textContent = player2.name;
+    player2health.textContent = player2.health;
+    player2energy.textContent = player2.energy;
+}
+
+// continuing on with battlefield-specific DOM elements...
 
 const player1Move = document.getElementById('player1Move');
 const player2Move = document.getElementById('player2Move');
@@ -38,28 +59,32 @@ const player1DefenseObject = findById(defense, player1.defendId);
 const player2AttackObject = findById(attacks, player2.attackId);
 
 if (turnOrder === 'player1First') {
-    player1Move.textContent = player1AttackObject.name;
-    player2Move.textContent = player2DefenseObject.name;
+    player1Move.textContent = `${player1.name} used ${player1AttackObject.name} against ${player2.name}!`;
+    player2Move.textContent = `${player2.name} defended with ${player2DefenseObject.name}.`;
     player1Description.textContent = player1AttackObject.description;
     player2Description.textContent = player2DefenseObject.description;
+    // calling renderStats() with other textContent manipulations, for clarity
+    renderStats();
 
     const arrayDamage = applyDamage(player1, player2);
     const randomRollNumber = arrayDamage[0];
     const numberNeeded = arrayDamage[2];
     const damageDealt = arrayDamage[1];
-    finalStats.textContent = 'the number the attacker rolled was ' + randomRollNumber + ' you needed ' + numberNeeded + ' to hit. The defender took ' + damageDealt + ' damage.';
+    finalStats.textContent = 'The number the attacker rolled was ' + randomRollNumber + ', and they needed ' + numberNeeded + ' to hit. The defender took ' + damageDealt + ' damage.';
 
 } else {
-    player1Move.textContent = player1DefenseObject.name;
-    player2Move.textContent = player2AttackObject.name;
+    player1Move.textContent = `${player1.name} defended with ${player1DefenseObject.name}.`;
+    player2Move.textContent = `${player2.name} used ${player2AttackObject.name} against ${player1.name}!`;
     player1Description.textContent = player1DefenseObject.description;
     player2Description.textContent = player2AttackObject.description;
+    // calling renderStats() with other textContent manipulations, for clarity
+    renderStats();
 
     const arrayDamage = applyDamage(player2, player1);
     const randomRollNumber = arrayDamage[0];
     const numberNeeded = arrayDamage[2];
     const damageDealt = arrayDamage[1];
-    finalStats.textContent = 'the number the attacker rolled was ' + randomRollNumber + ' you needed ' + numberNeeded + ' to hit. The defender took ' + damageDealt + ' damage.';
+    finalStats.textContent = 'The number the attacker rolled was ' + randomRollNumber + ', and they needed ' + numberNeeded + ' to hit. The defender took ' + damageDealt + ' damage.';
 }
 
 const buttonLink = document.getElementById('link-button');
