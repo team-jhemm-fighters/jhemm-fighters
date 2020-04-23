70d89d78c4055e0087f3dfee94267a83d0b2e891
Max Lamb <lordmaxlamb@gmail.com>
2020-04-23 16:18:11 -0700

    fixed blank interim page bug, refactored code

--- battlefield/interim.js
+++ battlefield/interim.js
@@ -1,25 +1,29 @@
-import { getLocalStorage, getTurnOne, getTurnTwo, setPlayerProfile } from '../common/utils.js';
+import { getLocalStorage, getTurnOne } from '../common/utils.js';
 import { turnOrder } from '../battlefield/battlefield-utils.js';
 //Pull Elements From HTML
 const navigateButton = document.getElementById('navigate-button');
 const nextPlayerSpan = document.getElementById('next-player-span');
 const player1 = getLocalStorage('player1');
 const player2 = getLocalStorage('player2');
+let turnOneComplete = false;
+let turnTwoComplete = false;
+// If we went to the battlefield and came back once
+turnOneComplete = getTurnOne();
 
 if (!player1.hasAttacked && !player2.hasAttacked) {
+    
     turnOrder();
 }
+
 // Get Player Data from Local Storage
 const turnPattern = getLocalStorage('TURN-PATTERN');
 
 // At the start of a round both turns should be false
-let turnOneComplete = false;
-let turnTwoComplete = false;
 
-// If we went to the battlefield and came back once
-turnOneComplete = getTurnOne();
-// If we go to the battle field and cam
-turnTwoComplete = getTurnTwo();
+
+// if (turnTwoComplete) {
+//     roundComplete();
+// }
 
 let link = '/interim.html';
 
@@ -30,22 +34,18 @@ if (turnPattern === 'player1First') {
         turn(player1, player2);
     } else if (turnOneComplete && !turnTwoComplete) {
         turn(player2, player1);
-    } else {
-        roundComplete();
-    }
+    } 
 } else {
     if (!turnOneComplete) {
         turn(player2, player1);
     } else if (turnOneComplete && !turnTwoComplete) {
         turn(player1, player2);
-    } else {
-        roundComplete();
-    }
+    } 
 }
 // Link gets updated in conditional turn logic
 navigateButton.addEventListener('click', () => {
     location.href = '/jhemm-fighters' + link;
-    //location.href = link;
+    // location.href = link;
     
 });
 
@@ -72,25 +72,25 @@ function turn(attacker, defender) {
     }
 }
 
-function roundComplete() {
-    
-    localStorage.removeItem('round1');
-    localStorage.removeItem('round2');
+// function roundComplete() {
+   
+//     localStorage.removeItem('round1');
+//     localStorage.removeItem('round2');
 
-    player1.hasAttacked = false;
-    player1.hasDefended = false;
+//     player1.hasAttacked = false;
+//     player1.hasDefended = false;
 
-    player2.hasDefended = false;
-    player2.hasAttacked = false;
+//     player2.hasDefended = false;
+//     player2.hasAttacked = false;
 
-    player1.attackId = '';
-    player1.defendId = '';
+//     player1.attackId = '';
+//     player1.defendId = '';
 
-    player2.attackId = '';
-    player2.defendId = '';
+//     player2.attackId = '';
+//     player2.defendId = '';
 
-    setPlayerProfile('player1', player1);
-    setPlayerProfile('player2', player2);
-    
+//     setPlayerProfile('player1', player1);
+//     setPlayerProfile('player2', player2);
 
-}
+//     turnOrder();
+// }
