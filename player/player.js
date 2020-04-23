761857ae73ad4614fe915cc2f5ac945c87300e4d
JoLeaper <joleap123@gmail.com>
2020-04-22 18:03:00 -0700

    added both player stats to move-selection page

--- player/player.js
+++ player/player.js
@@ -4,17 +4,24 @@ import attacks from '../data/attack.js';
 import defenses from '../data/defense.js';
 
 const button = document.getElementById('submit-action');
+const actionHtml = document.getElementById('actions');
+
+// player 1 HTML + getting
+const player1 = getLocalStorage('player1');
+const player1name = document.getElementById('player-1-name');
+const player1health = document.getElementById('player-1-health');
+const player1energy = document.getElementById('player-1-energy');
+
+// player 2 HTML + getting
+const player2 = getLocalStorage('player2');
+const player2name = document.getElementById('player-2-name');
+const player2health = document.getElementById('player-2-health');
+const player2energy = document.getElementById('player-2-energy');
+
 const searchParams = new URLSearchParams(window.location.search);
 const playerId = searchParams.get('id');
 const currentTurn = searchParams.get('turn');
-
-const playername = document.getElementById('name');
-const actionHtml = document.getElementById('actions');
-
 const currentPlayer = getLocalStorage(playerId);
-console.log(currentTurn);
-
-playername.textContent = currentPlayer.name;
 
 const chosenClass = findById(classes, currentPlayer.class);
 let actions;
@@ -26,6 +33,14 @@ if (currentTurn === 'attack') {
     actions = defenses;
     actionsArray = 'defActions';
 }
+
+player1name.textContent = player1.name;
+player1health.textContent = player1.health;
+player1energy.textContent = player1.energy;
+
+player2name.textContent = player2.name;
+player2health.textContent = player2.health;
+player2energy.textContent = player2.energy;
 
 
 for (let i = 0; i < chosenClass[actionsArray].length; i++) {
