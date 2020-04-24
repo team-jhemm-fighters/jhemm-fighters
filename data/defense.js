57373b275d99a7c3f73ecdff5f110697214f4cf9
smooto <melissasmoot@live.com>
2020-04-24 09:45:38 -0700

    add further action image fixes

--- data/defense.js
+++ data/defense.js
@@ -73,7 +73,7 @@ const casterLightningDodge = {
 const casterStoneWall = {
     id: 'action8',
     name: 'Stone Wall',
-    image: '/jhemm-fighters/assets/wallofstone.jpg',
+    image: '/jhemm-fighters/assets/wallofstone.png',
     description: 'You call upon the Earth to raise a wall of stone!.',
     defenseRating: 2,
     health: 0,
